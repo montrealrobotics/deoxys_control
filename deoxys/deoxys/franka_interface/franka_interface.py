4603ee9756d0a3df9ca4aab7cecb3eaf8edb2fbb
Kirsty Ellis <kirsty.ellis@mila.quebec>
2025-10-30 12:05:22 -0400

    edit action length

--- deoxys/deoxys/franka_interface/franka_interface.py
+++ deoxys/deoxys/franka_interface/franka_interface.py
@@ -385,7 +385,7 @@ class FrankaInterface:
         elif controller_type == "JOINT_POSITION":
 
             assert controller_cfg is not None
-            assert len(action) == 7 + 1
+            assert len(action) == 7
 
             joint_pos_msg = franka_controller_pb2.FrankaJointPositionControllerMessage()
             joint_pos_msg.speed_factor = 0.1
